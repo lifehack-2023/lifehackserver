8296789379276e3afb29624d525df710ef6397e8
mohamad-j <mohamad.webdev@gmail.com>
2024-01-18 14:48:58 +0100

    Fix the server.

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const path = require('path');
+const fs = require('fs');
+
 
 const express = require('express');
 const app = express();
 
 
-app.get('/',function( req, res ){
+app.get('/:filename?',function( req, res ){
 
-    res.sendFile(`${path.join(__dirname, `frontend/home.html` )}`);
+    let file_name = 'home';
+
+    if( !fs.existsSync( path.join( __dirname, `frontend/${req.params.filename}.html` ) ) ){
+        file_name = 'notfound';
+    } 
+
+    if( !req.params.filename  ) {
+        file_name = 'home';
+    }
+
+    res.sendFile(`${path.join(__dirname, `frontend/${file_name}.html` )}`);
 });
 
-app.get('/about',function( req, res ){
-
-    res.sendFile(`${path.join(__dirname, `frontend/about.html` )}`);
-});
-
-
-app.get('/schools',function( req, res ){
-
-    res.sendFile(`${path.join(__dirname, `frontend/school.html` )}`);
-});
 
 
 app.listen(3000, function(){
